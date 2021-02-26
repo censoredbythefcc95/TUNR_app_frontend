a3f1f053be1367e4768b8e291fc9a2c37befb05d
Steven <sastudillo6@gmail.com>
2021-02-26 15:23:09 -0500

    app.js structure, components structures set. front-end is displaying, but API needs to be linked.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// REACT IMPORTS--------------------------------------
+import React, {useState, useEffect} from 'react'
+// AXIOS IMPORTIMPORTS--------------------------------------
+import axios from 'axios'
+// COMPONENT IMPORTS ---------------------------------
+import Titlebar from "./components/TopBar";
+import Playlist from "./components/Playlist";
+import FavSong from "./components/FavoriteSongs";
+import NewSongs from "./components/NewSongs";
 
 function App() {
+
+  // define backend url base------------------------------------
+  const url = "https://tunrappapi.herokuapp.com"
+  // Defines STATES------------------------------------
+  const [playlist, setPlaylist] = useState([])
+  const [selectedSong, setSelectedSong] = useState("")
+  // CRUD ROUTES------------------------------------
+  // GET - INDEX---
+  const getSongs = () => {
+    axios.get(url + "/songs").then((res) => {
+      const songs = res.data;
+      setPlaylist(songs);
+    });
+  };
+  useEffect(() => {
+    getSongs();
+  }, []);
+  
+  // CREATE - ADD A SONG VIA FORM----
+  const handleCreateSong = (newSong) => {
+    axios.post(url +'/songs', newSong)
+    .then((res) => {
+      setPlaylist(...playlist, newSong);
+    })
+    .then(() => getSongs());
+  };
+  // DESTROY ---
+  
+  const deleteSong = (song)=>{
+    console.log("this is song",song)
+      axios.delete(url + '/songs/' + song._id)
+      .then((res)=>{getSongs()})
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Titlebar />
+      <div className="playlist-name">
+        <h2> Playlist 1</h2>
+      </div>
+      <Playlist 
+        playlist={playlist} //Pass Down playlist state into playlist comp
+        deleteSong = {deleteSong}
+
+      />
+      <FavSong />
+      <NewSongs playlist={playlist} create={handleCreateSong} />
     </div>
   );
 }
---
+++ src/components/Playlist.js
@@ -0,0 +1,37 @@
+
+import React from "react";
+
+const Playlist = (props) => {
+  const { playlist, deleteSong } = props;
+  const loaded = () => {
+    return (
+      <div key={Playlist._id}>
+        {playlist.map((song) => {
+          return (
+            <article>
+              
+
+              <div className="song-name">
+                <p>{song.title}</p>
+              </div>
+
+              <div className="artist-name">
+                <p> {song.artist} </p>
+              </div>
+
+              <div className="song-length">
+                <p> {song.time}</p>
+              </div>
+              <button onClick={()=>{deleteSong(song)}}>X</button>
+            </article>
+          );
+        })}
+      </div>
+    );
+  };
+  const loading = <h6>What are you Listening to?</h6>;
+
+  return playlist.length >0 ? loaded(): loading;
+};
+
+export default Playlist;
